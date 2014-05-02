c3f62897ac007f355589901ba9376dec4bb2b68a
Luke <lucasmccomb@gmail.com>
2014-05-02 10:33:57 -0600

    shoplist debug

--- shopList/js/app.js
+++ shopList/js/app.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
-	//text input
+//signature hello world
 	console.log('Smells like updog in here');
-	
+//Declaring global var
 	var items = 0;
-
+//Gathering data from input on pressing 'enter'
 	function enterItem() {
 		$("#newitem").bind('keypress', function(e) {
 			if (e.keyCode == 13) {
@@ -11,13 +11,13 @@ $(document).ready(function() {
 			}
 		});
 	}
-
+//run
 	enterItem();
-
+//add data as item of list, must have value
 	function addItem() {
 		var item = $("#newitem").val();
 		var addli = '<li class="need"><img src="images/check.png" class="check">' + item + '<img src="images/x.png" class="x"></li>';
-		if(item.length > 0){            
+		if(item.length > 0) {            
 			$("#list").append(addli);
 			$("#newitem").val('');
 		}
@@ -25,18 +25,25 @@ $(document).ready(function() {
 			alert("What do you need?");
 		}
 	}
-
+//reset button
 	$(document).on("click", "#reset", function() {
 		$("#list").empty();
 		items = 0;
 	});
-
+//strike through
 	$(document).on("click", ".check", function() {
 		$(this).closest('li').toggleClass("strike");
 		items = 0;
 	}); 
-
+//remove
 	$(document).on("click", ".x", function() {
 		$(this).closest('li').fadeOut(300);
 	});
+//mouseover logo animate class change
+	$('i img').on('mouseenter',function() {
+        $('.logo').addClass('logoh');
+    });
+    $('i img').on('mouseleave',function() {
+        $('.logo').removeClass('logoh');
+    });
 });
