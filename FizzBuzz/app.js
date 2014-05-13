3fa85e201884cb1de6492102b6f54971b84d8af6
Luke <lucasmccomb@gmail.com>
2014-05-13 16:30:01 -0600

    final FizzBuzz

--- FizzBuzz/app.js
+++ FizzBuzz/app.js
@@ -1,22 +1,37 @@
 $(document).ready(function() {
+    
+    function addNum() {
+        var num = $("#num").val();
+        if(num.length > 0 || num % 1 === 0) {            
+            for (var i = 1; i <= num; i++) {
+                 if (i % 3 === 0 ) {
+                     if (i % 5 === 0) {
+                         document.write("<p>FizzBuzz</p>");
+                     } else {
+                         document.write("<p>Fizz</p>");
+                     }   
+                 } else if (i % 5 === 0) {
+                     if (i % 3 === 0) {
+                         document.write("<p>FizzBuzz</p>");
+                     } else {
+                         document.write("<p>Buzz</p>");
+                     }
+                 } else {
+                     document.write("<p>" + i + "</p>");
+                 }
+             }
+        }
+        else {
+            alert("Please Enter a number and press enter");
+        }
+    }
 
-		for (var i = 1; i < 101; i++) {
-			if (i % 3 === 0 ) {
-				if (i % 5 === 0) {
-					document.write("<p>FizzBuzz</p>");
-				} else {
-					document.write("<p>Fizz</p>");
-				}   
-			} else if (i % 5 === 0) {
-				if (i % 3 === 0) {
-					document.write("<p>FizzBuzz</p>");
-				} else {
-					document.write("<p>Buzz</p>");
-				}
-			} else {
-				document.write("<p>" + i + "</p>");
+    function enterItem() {
+		$("#num").bind('keypress', function(e) {
+			if (e.keyCode == 13) {
+				addNum();
 			}
-			
-		}
-		
-});
+		});
+	}
+	enterItem();       
+});     
